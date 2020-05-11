bb14631d4da3df196b983526be0a75264e8cb25d
cfei9 <cfei9@gatech.edu>
2020-05-11 13:57:34 -0400

    delete some files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state ={
+      data:{
+        labels:["1","2","3","4","5"],
+        datasets: [
+          {
+            label: "videos Mades",
+            backgroundColor: "rgba(255,0,255,0.75)",
+            data:[4,5,1,10,32,2,12]
+          },
+          {
+            label:"Subscriptions",
+            backgroundColor:"rgba(0,255,0,0.75)",
+            data:[14,15,21,0,12,24,32]
+
+          }
+        ]
+      }
+    }
+  }
+
+  setGradientColor = (canvas, color) => {
+    const ctx = canvas.getContext('2d');
+    const gradient = ctx.createLinearGradient(0,0,0,400);
+    gradient.addColorStop(0,color);
+    gradient.addColorStop(0.95,"rgba(133,255,144,0.85)");
+    return gradient;
+
+  }
+
+
+  getChartData = canvas => {
+    const data = this.state.data;
+    if (data.datasets){
+      let colors = ["rgba(255,0,255,0.75)","rgba(0,255,0,0.75)"];
+      data.datasets.forEach((set,i) => {
+        set.backgroundColor = this.setGradientColor(canvas, colors[i]);
+        set.borderColor = "white";
+        set.borderWidth = 2;
+      });
+    }
+
+    return data;
+  }
+
+
+
+  render(){
+    return(
+      <div style={{position: "relative", width: 600, height: 550}}>
+        <h3> Chart Samples </h3>
+        <Line
+          options={{
+            reponsive: true
+          }}
+          data={this.getChartData}
+        />
+      </div>
+    )
+  }
 }
-
-export default App;
